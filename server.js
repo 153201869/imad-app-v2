e1f0252dbdb4ce847adc34403371ef1c837d0cff
PUNEET <sharmapuneet557@gmail.com>
2017-02-13 04:52:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/aboutme',function(req,res){
-    res.sendFile(path.join(_dirname,'about_me.html'));
+    res.sendFile(path.join(_dirname,'ui','about_me.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
