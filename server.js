13f63cc44185983ae392d0862b985e9f3f541b8b
PUNEET <sharmapuneet557@gmail.com>
2017-02-13 05:44:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/index.html',function(req,res){
+app.get('/ui/index.html',function(req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
